6085b507128763552981cf8e7ab8c903db77e819
Peez-Gloriousintel <apzku69.tutor@gmail.com>
2016-03-26 22:55:21 +1100

    edited install.sh to dynamically set Perl path

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 PWD=`pwd`
 BIN="/usr/local/bin"
+PERL=`which perl`
+if [ "$PERL" != "" ]; then
+  sed -i '' "s#PATH_TO_PERL#${PERL}#" msockstat.perl
+else
+  echo "Installation failed -- Perl not found..."
+  exit 1
+fi
 
 chmod +x msockstat.perl
 ln -s $PWD/msockstat.perl $BIN/msockstat && \
